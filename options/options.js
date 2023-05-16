d33d72cf8059087a6fa19abf5586c5a0402edf09
Crowdbotics <team@crowdbotics.com>
2023-05-16 18:00:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flurry-40738",
+  url: "https://flurry-40738.botics.co",
+  api: "https://flurry-40738.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
